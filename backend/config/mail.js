6faf168d9c12839ada0ecc17a0dbb33364a7dfcd
AnonJeffz <gibemark143@gmail.com>
2026-02-23 18:57:42 +0800

    Fix: Check SENDGRID_API_KEY at runtime, not import time

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,14 +1,14 @@
 import sgMail from '@sendgrid/mail';
 
-if (!process.env.SENDGRID_API_KEY) {
-  throw new Error('SENDGRID_API_KEY is not set');
-}
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
 const mail = {
   
   sendMail: async (details) => {
+    if (!process.env.SENDGRID_API_KEY) {
+      throw new Error('SENDGRID_API_KEY is not set');
+    }
+    
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    
     const msg = {
       to: details.to,
       from: details.from || process.env.SENDGRID_FROM,
